484df256030b48c86c84f1ceac2370ca8ee245eb
ckr91910-design <ckr91910@gmail.com>
2026-02-16 21:56:34 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
+/**
+ * ReactDOM.createRoot: 
+ * HTML의 <div id="root"></div> 요소를 찾아 그 안에 React 앱을 렌더링합니다.
+ */
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
